bc07e8a2ed117f7e66a01d8f6a4e12618624dc3c
LuckDev <lukedru@yahoo.com>
2023-03-24 10:48:17 -0700

    Initial Commit

---
+++ BouncyBallDemo/BouncyBallDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BouncyBallDemoApp.swift
+//  BouncyBallDemo
+//
+//  Created by Luke Drushell on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct BouncyBallDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
